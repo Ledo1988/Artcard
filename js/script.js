c8ea415cacac9d23eae6cd612d697e787a787bf3
Elena <elena.do1988@yandex.ru>
2019-11-01 18:58:01 +0300

    main-slider, main-about are added

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$( document ).ready(function() {
+	var mainSwiper = new Swiper('.main-swiper', {
+		spaceBetween: 0,
+		centeredSlides: true,
+		loop: true,
+		autoplay: {
+			delay: 2500,
+			disableOnInteraction: false,
+		},
+	});
+});
