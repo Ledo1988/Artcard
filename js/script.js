faf377bc15e746d37a43c54b2d944eb1e3dc3e98
Elena <elena.do1988@yandex.ru>
2019-11-04 03:41:47 +0300

    main-details is added

--- js/script.js
+++ js/script.js
@@ -8,4 +8,16 @@ $( document ).ready(function() {
 			disableOnInteraction: false,
 		},
 	});
+
+	$('body .main-details-tabs').on('click', '.tab-link', function(){
+		var tab_id = $(this).attr('data-tab');
+
+		$('.main-details-tabs .tab-link').removeClass('current');
+		$('.tab-content').removeClass('current');
+
+		$(this).addClass('current');
+		$("#"+tab_id).addClass('current');
+
+
+	})
 });
