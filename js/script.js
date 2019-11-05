85a93f84ec6740eb9a3da2773c9cc3c96116fe9d
Elena <elena.do1988@yandex.ru>
2019-11-05 17:22:23 +0300

    form is added

--- js/script.js
+++ js/script.js
@@ -17,7 +17,42 @@ $( document ).ready(function() {
 
 		$(this).addClass('current');
 		$("#"+tab_id).addClass('current');
+	});
 
+	$('body .main-order-form').on('click', '.main-order-forward', function(){
+		var stageCurrent = $(this).parents('.main-order-form').data('stage');
+		if (stageCurrent < 5) {stageCurrent++;}
 
-	})
+		$(this).parents('.main-order-form').data('stage', stageCurrent);
+
+		$(this).parents('.main-order-form').find('.main-order-form-stage .current').html(stageCurrent);
+
+		$(this).parents('.main-order-form').find('.main-order-form-item').removeClass('active');
+		$(this).parents('.main-order-form').find(`[data-item='${stageCurrent}']`).addClass('active');
+
+		if (stageCurrent == 1) {
+			$('body .main-order-form').find('.main-order-back').addClass('inactive');
+		} else {
+			$('body .main-order-form').find('.main-order-back').removeClass('inactive');
+		}
+	});
+
+	$('body .main-order-form').on('click', '.main-order-back', function(){
+		var stageCurrent = $(this).parents('.main-order-form').data('stage');
+		if (stageCurrent > 1) {stageCurrent--;}
+		$(this).parents('.main-order-form').data('stage', stageCurrent);
+
+		$(this).parents('.main-order-form').find('.main-order-form-stage .current').html(stageCurrent);
+
+		$(this).parents('.main-order-form').find('.main-order-form-item').removeClass('active');
+		$(this).parents('.main-order-form').find(`[data-item='${stageCurrent}']`).addClass('active');
+
+		if (stageCurrent == 1) {
+			$('body .main-order-form').find('.main-order-back').addClass('inactive');
+		} else {
+			$('body .main-order-form').find('.main-order-back').removeClass('inactive');
+		}
+
+	});
+
 });
