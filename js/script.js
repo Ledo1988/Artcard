7d3b60aff88ba0468128f0154b6d123be2bc02eb
Elena <elena.do1988@yandex.ru>
2019-11-07 19:03:07 +0300

    mobile started

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-$( document ).ready(function() {
+$(document).ready(function() {
 	var mainSwiper = new Swiper('.main-swiper', {
 		spaceBetween: 0,
 		centeredSlides: true,
@@ -7,6 +7,19 @@ $( document ).ready(function() {
 			delay: 2500,
 			disableOnInteraction: false,
 		},
+	});
+
+	$('.date').mask('00.00.0000');
+	$('.tel').mask('+7 (000) 000 - 00 - 00');
+	$('.passport').mask('0000 000000');
+	$('.code').mask('000 000');
+
+	$(document).on('click', 'a[href^="#"]', function (event) {
+		event.preventDefault();
+
+		$('html, body').animate({
+			scrollTop: $($.attr(this, 'href')).offset().top
+		}, 500);
 	});
 
 	$('body .main-details-tabs').on('click', '.tab-link', function(){
@@ -19,9 +32,14 @@ $( document ).ready(function() {
 		$("#"+tab_id).addClass('current');
 	});
 
-	$('body .main-order-form').on('click', '.main-order-forward', function(){
+	$('body .main-order-form .btn-wrapper').on('click', '.btn', function(){
 		var stageCurrent = $(this).parents('.main-order-form').data('stage');
-		if (stageCurrent < 5) {stageCurrent++;}
+
+		if ($(this).hasClass('main-order-forward')) {
+			if (stageCurrent < 5) {stageCurrent++;}
+		} else if ($(this).hasClass('main-order-back')) {
+			if (stageCurrent > 1) {stageCurrent--;}
+		}
 
 		$(this).parents('.main-order-form').data('stage', stageCurrent);
 
@@ -37,22 +55,8 @@ $( document ).ready(function() {
 		}
 	});
 
-	$('body .main-order-form').on('click', '.main-order-back', function(){
-		var stageCurrent = $(this).parents('.main-order-form').data('stage');
-		if (stageCurrent > 1) {stageCurrent--;}
-		$(this).parents('.main-order-form').data('stage', stageCurrent);
 
-		$(this).parents('.main-order-form').find('.main-order-form-stage .current').html(stageCurrent);
 
-		$(this).parents('.main-order-form').find('.main-order-form-item').removeClass('active');
-		$(this).parents('.main-order-form').find(`[data-item='${stageCurrent}']`).addClass('active');
 
-		if (stageCurrent == 1) {
-			$('body .main-order-form').find('.main-order-back').addClass('inactive');
-		} else {
-			$('body .main-order-form').find('.main-order-back').removeClass('inactive');
-		}
-
-	});
 
 });
